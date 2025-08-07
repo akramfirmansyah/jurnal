b385db6b12811066e67c6919e95beb20fa84ca5e
akramfirmansyah <akram.firman@gmail.com>
2025-08-07 16:47:30 +0800

    add completion message to setup script for clarity

--- be-server/setup.sh
+++ be-server/setup.sh
@@ -6,3 +6,6 @@ docker build -t flask-app .
 
 # Run the Docker container
 docker run -d -p 5000:5000 flask-app --name flask-container
+
+# Print a message indicating that the setup is complete
+echo "Flask application is running in a Docker container on port 5000."
