643cbb21c844a98c4a221ec8075ca38d19c10069
akramfirmansyah <akram.firman@gmail.com>
2025-08-07 16:45:53 +0800

    fixed setup.sh

--- be-server/setup.sh
+++ be-server/setup.sh
@@ -1,5 +1,8 @@
+# remove the old Docker container if it exists
+docker rm -f flask-container || true
+
 # Build the Docker image
 docker build -t flask-app .
 
 # Run the Docker container
-docker run -d -p 5000:5000 flask-app
+docker run -d -p 5000:5000 flask-app --name flask-container
